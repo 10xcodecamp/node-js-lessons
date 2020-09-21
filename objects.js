7bcdf5aa4016b13c2eba0e8d5941311e365f1436
149203 <149203@gmail.com>
2020-09-21 16:08:02 -0700

    134A Introduction to Objects

---
+++ objects.js
@@ -0,0 +1,30 @@
+const user = {
+   email: getEmail(),
+   password: getPassword(),
+   createdAt: getCreatedAt(),
+   id: getId(),
+};
+
+console.log(user);
+
+/* functions start here */
+
+function getEmail() {
+   // get the user's email from the input with jQuery
+   return "mike@gmail.com";
+}
+
+function getPassword() {
+   // get the user's password from the input with jQuery
+   return "skywalker83";
+}
+
+function getCreatedAt() {
+   // generate createdAt number, e.g. 20200921
+   return 20200921;
+}
+
+function getId() {
+   // generate id string, e.g. "005061"
+   return "005061";
+}
