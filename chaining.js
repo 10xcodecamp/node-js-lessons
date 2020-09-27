aa2de49b959576444c31ff8b06a756c078c58436
149203 <149203@gmail.com>
2020-09-27 13:17:16 -0700

    start chaining.js

---
+++ chaining.js
@@ -0,0 +1,63 @@
+const users = [
+   {
+      name: "Mike",
+      isActive: true,
+      createdAt: 1601234512420,
+      socialProfiles: [
+         {
+            site: "twitter",
+            username: "mzetlow",
+         },
+         {
+            site: "facebook",
+            username: "mikezetlow",
+            profilePhoto: {
+               sm: "small.jpg",
+               lg: "large.jpg",
+            },
+         },
+      ],
+   },
+   {
+      name: "John",
+      isActive: true,
+      createdAt: 1601234512430,
+      socialProfiles: [
+         {
+            site: "facebook",
+            username: "jsmith",
+            profilePhoto: {
+               sm: "small.jpg",
+               lg: "large.jpg",
+            },
+         },
+      ],
+   },
+   {
+      name: "Michelle",
+      isActive: false,
+      createdAt: 1601234512450,
+      socialProfiles: [
+         {
+            site: "twitter",
+            username: "michelledoe",
+         },
+      ],
+   },
+];
+
+const fbUsers = users.map((user) => {
+   // name * fb name only
+   const newUser = {
+      name: user.name,
+      fbName: getFbName(user),
+   };
+   return newUser;
+});
+
+function getFbName(user) {
+   // return FB name if they have one
+   // if they don't return empty string: ""
+}
+
+console.log(fbUsers);
