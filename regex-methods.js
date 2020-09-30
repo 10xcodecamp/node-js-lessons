0e1238e8a00e6fe3af755541bd2ac17d391942fb
149203 <149203@gmail.com>
2020-09-30 14:54:00 -0700

    📹 145A The Regex test Method

--- regex-methods.js
+++ regex-methods.js
@@ -1,1 +1,4 @@
-const regex = /REGEX/gi;
+const phonePattern = /^\(\d{3}\)\s\d{3}-\d{4}$/;
+const userInput = "(517) 555-1234";
+const isValidPhone = phonePattern.test(userInput);
+// console.log(isValidPhone);
